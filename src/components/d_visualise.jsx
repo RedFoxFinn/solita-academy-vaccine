6556632b2badf0cb6d131cdce2254083f682f277
RedFoxFinn <laborantti.antti@gmail.com>
2021-06-27 18:07:34 +0300

    feat: added data sorting to r_orders

--- src/components/d_visualise.jsx
+++ src/components/d_visualise.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import Plot from 'react-plotly.js';
 
-const DataVisualisation = (props) => {
-  const {vaccinations, vaccines} = props;
+const DataVisualisation = ({vaccinations, orders}) => {
+  const vData = vaccinations;
+  const oData = orders;
+
+  function sortVaccinations(by = 'default', direction = 'asc') {
+    switch (by) {
+      default: vData.sort((a, b) => {
+        return a.vaccinationId < b.vaccinationId ? 1 : -1;
+      });
+    }
+  }
+  function sortOrders(by = 'default', direction = 'asc') {
+    switch(by) {
+      default: oData.sort((a, b) => {
+        return a.id < b.id ? 1 : -1;
+      });
+    }
+  }
   return <section id='dataVisualisation' >
     <p>data visualisation</p>
+    <article style={{outline: '1px solid rgb(220,30,50)'}}>
+      <Plot
+        data={[
+          {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: {color: 'red'},
+          },
+          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+        ]}
+        layout={ {width: window.innerWidth/2, height: window.innerHeight/2, title: 'A Fancy Plot'} }
+      />
+    </article>
   </section>
 };
 
--- src/components/r_orders.jsx
+++ src/components/r_orders.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {Vaccine} from './vaccine';
 
 const RenderOrders = ({orders}) => {
+  const [sorting, setSorting] = useState('oNumA');
+  const setSortingOrder = (order) => {
+    switch (order) {
+      case 'orderNumberA': setSorting('oNumA'); break;
+      case 'orderNumberD': setSorting('oNumD'); break;
+      default: setSorting('oNumA');
+    }
+  }
+  const getSorted = () => {
+    const data = orders;
+    switch (sorting) {
+      case 'oNumA': return data;
+      case 'oNumD': return data.sort((a,b) => a.orderNumber < b.orderNumber ? 1 : -1);
+      default: return data;
+    }
+  };
   return <section id='orders' >
     {orders
-      ? orders.map(vaccine => <Vaccine embedded={false} {...vaccine} />)
+      ? getSorted().map(vaccine => <Vaccine embedded={false} {...vaccine} />)
       : <p>Orders</p>}
   </section>
 };
